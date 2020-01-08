f0e7daa7d771315800e241dec63af0bd8376d758
Chris Worman <chrisworman@gmail.com>
2020-01-08 01:29:26 -0800

    GET supports dateFilter

--- routes/reports.js
+++ routes/reports.js
@@ -4,8 +4,20 @@ const Report = require('../models/report');
 
 router.get('/', async (req, res) => {
   try {
-    const reports = await Report.find();
-    res.json(reports);
+    var filter = {};
+    const dateFilter = req.query.dateFilter;
+    if (dateFilter) {
+      if (dateFilter === '24h') {
+        filter.createdOn = {
+            $gte: new Date(new Date().setDate(new Date().getDate()-1))
+        };
+      } else {
+        return res.status(400).json({ message: 'Unrecognized dateFilter: ' + dateFilter });
+      }
+    }
+    console.log('report filter: ');
+    console.log(filter);
+    res.json(await Report.find(filter));
   } catch (err) {
     console.log(JSON.stringify(err));
     res.status(500).json({ message: err.message });
